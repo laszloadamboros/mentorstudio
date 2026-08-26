7cf63450939f2b1488d5bba7df3f5a4c212d7f82
laszloadamboros <boroslaszloadam@icloud.com>
2026-08-26 23:05:57 +0200

    Fix CORS policy for Vercel deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+app.use(cors());
 const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
